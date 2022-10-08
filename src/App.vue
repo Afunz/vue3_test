833d6ca6cdcb9dfd83e0a11379f169eefbb58f38
zyy <269530843@qq.com>
2022-10-08 20:27:18 +0800

    第七次提交

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,25 @@
 <template>
-  <h1>个人信息</h1>
-  <h2>姓名:{{person.name}}</h2>
-  <h2>年龄:{{person.age}}</h2>
-  <h2>工作类型:{{person.job.type}}</h2>
-  <h2>工资:{{person.job.salary}}</h2>
-  <h2>爱好:{{person.hobby}}</h2>
-  <button @click="changeInfo">修改人员信息</button>
- 
+<Demo msg="测试" school="尚硅谷" @hello="showEmit">
+<span>尚硅谷</span>
+</Demo>
 </template>
 
 <script>
-// import { h } from 'vue'
-import { reactive } from 'vue'
 
 
+
+import Demo from './components/Demo'
 export default {
   name: 'App',
+  components: { Demo },
   setup() {
-    let {person}= reactive(
-      {
-        person:  {
-        name:'张三',
-        age : 18,
-        job :{
-        type: "前端工程师",
-        salary:"10K"
-        },
-        hobby:['打游戏','吃饭']
+    function showEmit(value) {
+      alert(`触发了showEmit,收到的参数是${value}`)
     }
-      }
-    )
-
-    function changeInfo() {
-      person.name = "李四"
-      person.age= 48
-     
-      person.job.type="UI设计师"
-      person.job.salary = "8k"
-      person.hobby[0] = "睡觉"
-       
+    return {
+      showEmit
     }
-
-   return{
-    person,
-    changeInfo
-   } 
-  // return ()=>h('h1','尚硅谷')
-  }  
+  }
 }
 </script>
 
---
+++ src/components/Demo.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1>个人信息</h1>
+  <h2>姓名:{{person.name}}</h2>
+  <h2>年龄:{{person.age}}</h2>
+  <h2>{{msg}}</h2>
+  <h2>{{school}}</h2>
+  <button @click="test">测试一下Demo组件的内容</button>
+  <slot></slot>
+</template>
+
+<script>
+// import { h } from 'vue'
+import { reactive } from 'vue'
+
+
+export default {
+  name: 'Demo',
+  props: ['msg', 'school'],
+  emits: ['hello'],
+  setup(props, context) {
+    console.log('--setup--',props);
+    console.log('--setup--',context);
+    let {person}= reactive(
+      {
+        person:  {
+        name:'张三',
+        age : 18,
+    }
+      }
+    )
+    function test() {
+      context.emit('hello',666)
+    }
+   return{
+     person,
+     test
+   } 
+  // return ()=>h('h1','尚硅谷')
+  }  
+}
+</script>
+
+<style>
+
+</style>
