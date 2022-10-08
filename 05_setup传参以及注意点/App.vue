90f5d6945584d3ea5d012c224a4ff3932b353103
zyy <269530843@qq.com>
2022-10-08 20:56:55 +0800

    第八次提交

---
+++ 05_setup传参以及注意点/App.vue
@@ -0,0 +1,28 @@
+<template>
+<Demo msg="测试" school="尚硅谷" @hello="showEmit">
+<span>尚硅谷</span>
+</Demo>
+</template>
+
+<script>
+
+
+
+import Demo from './components/Demo'
+export default {
+  name: 'App',
+  components: { Demo },
+  setup() {
+    function showEmit(value) {
+      alert(`触发了showEmit,收到的参数是${value}`)
+    }
+    return {
+      showEmit
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
