684664f42fc552827a90e57cab78790df5c2e009
zyy <269530843@qq.com>
2022-10-08 16:41:20 +0800

    第五次提交

---
+++ 03_ref函数/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <h1>个人信息</h1>
+  <h2>姓名:{{name}}</h2>
+  <h2>年龄:{{age}}</h2>
+  <h2>工作类型:{{job.type}}</h2>
+  <h2>工资:{{job.salary}}</h2>
+  <button @click="changeInfo">修改人员信息</button>
+ 
+</template>
+
+<script>
+// import { h } from 'vue'
+import { ref} from 'vue'
+
+
+export default {
+  name: 'App',
+  setup() {
+    let name=ref('张三')
+    let age = ref(18)
+    let job = ref({
+      type: "前端工程师",
+      salary:"10K"
+    })
+
+    function changeInfo() {
+      name.value = "李四"
+      age.value = 48
+      job.value.type="UI设计师"
+      job.value.salary="8k"
+    }
+
+   return{
+    name,
+     age,
+    job,
+    changeInfo
+   } 
+  // return ()=>h('h1','尚硅谷')
+  }  
+}
+</script>
+
+<style>
+
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,46 @@
 <template>
   <h1>个人信息</h1>
-  <h2>姓名:{{name}}</h2>
-  <h2>年龄:{{age}}</h2>
-  <h2>工作类型:{{job.type}}</h2>
-  <h2>工资:{{job.salary}}</h2>
+  <h2>姓名:{{person.name}}</h2>
+  <h2>年龄:{{person.age}}</h2>
+  <h2>工作类型:{{person.job.type}}</h2>
+  <h2>工资:{{person.job.salary}}</h2>
+  <h2>爱好:{{person.hobby}}</h2>
   <button @click="changeInfo">修改人员信息</button>
  
 </template>
 
 <script>
 // import { h } from 'vue'
-import { ref } from 'vue'
+import { reactive } from 'vue'
 
 
 export default {
   name: 'App',
   setup() {
-    let name=ref('张三')
-    let age = ref(18)
-    let job = ref({
-      type: "前端工程师",
-      salary:"10K"
-    })
+    let person = reactive(
+      {
+      name:'张三',
+      age : 18,
+      job :{
+        type: "前端工程师",
+        salary:"10K"
+        },
+      hobby:['打游戏','吃饭']
+    }
+    )
 
     function changeInfo() {
-      name.value = "李四"
-      age.value = 48
-      job.value.type="UI设计师"
-      job.value.salary="8k"
+      person.name = "李四"
+      person.age= 48
+     
+      person.job.type="UI设计师"
+      person.job.salary = "8k"
+      person.hobby[0] = "睡觉"
+       
     }
 
    return{
-    name,
-     age,
-    job,
+    person,
     changeInfo
    } 
   // return ()=>h('h1','尚硅谷')
